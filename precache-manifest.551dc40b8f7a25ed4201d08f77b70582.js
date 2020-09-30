bc0c17daff63c88f164f92769418cbe181c5b6f5
bertoldo <matheus.bertoldosi@gmail.com>
2020-09-30 18:22:58 -0500

    Updates

---
+++ precache-manifest.551dc40b8f7a25ed4201d08f77b70582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc7fc39a8d24ece841bca4ee3b4a69b",
+    "url": "/cypress-github-actions/index.html"
+  },
+  {
+    "revision": "142ba527d3bb7fe78e68",
+    "url": "/cypress-github-actions/static/css/main.774aecb0.chunk.css"
+  },
+  {
+    "revision": "531cdbdd4cc6a9042204",
+    "url": "/cypress-github-actions/static/js/2.252a7dcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cypress-github-actions/static/js/2.252a7dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142ba527d3bb7fe78e68",
+    "url": "/cypress-github-actions/static/js/main.22548f20.chunk.js"
+  },
+  {
+    "revision": "470e419fcb4dbe56be2a",
+    "url": "/cypress-github-actions/static/js/runtime-main.f48ae138.js"
+  }
+]);
